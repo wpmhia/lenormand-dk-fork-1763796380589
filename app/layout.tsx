6e2054f531b126c2a9f5acde77490875139e91b0
Ideavo AI <admin@ideavo.ai>
2025-11-22 14:53:01 +0000

    Add Google Analytics (GA4) tracking with measurement ID G-WDLWCCJCY8

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,6 +94,23 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WDLWCCJCY8"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WDLWCCJCY8');
+            `,
+          }}
+        />
       </head>
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider>
