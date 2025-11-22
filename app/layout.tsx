05052057f96a7a6e91a6d8c197b2f2a88ea0a58f
Ideavo AI <admin@ideavo.ai>
2025-11-22 15:13:55 +0000

    Commit by Ideavo AI

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,7 +95,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-WDLWCCJCY8"
+          src="https://www.googletagmanager.com/gtag/js?id=G-ESFQHZSKLQ"
           strategy="afterInteractive"
         />
         <Script
@@ -106,7 +106,25 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-WDLWCCJCY8');
+              gtag('config', 'G-ESFQHZSKLQ');
+            `,
+          }}
+        />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtm.js?id=GT-KTTDM7CZ"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtm-noscript"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GT-KTTDM7CZ');
             `,
           }}
         />
