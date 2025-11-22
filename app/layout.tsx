fb61c25e4ef32e351073309807fd493564c092ec
Ideavo AI <admin@ideavo.ai>
2025-11-22 14:54:28 +0000

    Remove duplicate X-UA-Compatible meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,7 +93,6 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-WDLWCCJCY8"
